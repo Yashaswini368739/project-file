a82fa05e04684d6950066d7083c1cf8a5e326b9a
Yashaswini368739 <yashaswiniyashaswini565@gmail.com>
2025-06-12 16:22:19 +0530

    Add files via upload

---
+++ colorpicker2.js
@@ -0,0 +1,23 @@
+const selectedColorHexCode = document.getElementById("selectedColorHexCode");
+function greybg() {
+
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    selectedColorHexCode.textContent = "#e0e0e0";
+}
+function greenbg() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    selectedColorHexCode.textContent = "#6fcf97";
+    
+}
+function skybluebg() {
+
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    selectedColorHexCode.textContent = "#56ccf2";
+    
+}
+function purplebg() {
+
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    selectedColorHexCode.textContent = "#bb6bd9";
+   
+}
